058eb53dbfcb7682e2aaa96c3501b742bb781aa5
PYN <yxp7531540@gmail.com>
2019-04-04 14:49:12 -0500

    passed

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all.each {|dog| puts dog.name}
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+
+
+
+
+
+
+
+end
